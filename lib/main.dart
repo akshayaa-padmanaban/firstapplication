0ce162ff5fd818d164ad79a18d73246a8acf706e
akshayaapadmanaban@gmail.com <akshayaapadmanaban@gmail.com>
2025-05-02 16:47:35 +0530

    First Application

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: LoginScreen(),
+  ));
+}
+
+class LoginScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              height: 50,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xFFB993D6), Color(0xFF8CA6DB)],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+            ),
+          ),
+
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 50,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xFFB993D6), Color(0xFF8CA6DB)],
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              width: 250,
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+              decoration: BoxDecoration(
+                color: Color(0xFFB993D6).withOpacity(0.7),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const SizedBox(height: 20),
+                  _inputField(Icons.person, "User Name"),
+                  const SizedBox(height: 16),
+                  _inputField(Icons.lock, "Password", obscureText: true),
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      const Spacer(),
+                      const Text("Forgot Password", style: TextStyle(color: Colors.black)),
+                    ],
+                  ),
+                  const SizedBox(height: 24),
+                  Container(
+                    width: double.infinity,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      color: Color(0xFF8CA6DB).withOpacity(0.4),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "LOGIN",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.2,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _inputField(IconData icon, String hint, {bool obscureText = false}) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: TextField(
+        obscureText: obscureText,
+        style: const TextStyle(color: Colors.black),
+        decoration: InputDecoration(
+          prefixIcon: Icon(icon, color: Colors.black),
+          hintText: hint,
+          hintStyle: const TextStyle(color: Colors.black),
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
